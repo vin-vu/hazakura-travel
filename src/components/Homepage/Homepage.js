ecab856c87dbd53d4e03f1c846967ebe50baa1f5
vin-vu <vince.hvu@gmail.com>
2021-11-15 21:36:29 -0800

    Align centered play button

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,6 +1,7 @@
 import "./Homepage.css";
 import Navbar from "./Navbar";
 import Button from "./Button";
+import PlayIcon from "../../icon-components/PlayIcon";
 
 // animation game plan: when scroll to new page, text delays and then gets stamped on
 
@@ -17,14 +18,22 @@ function Homepage() {
         </p>
         <p className="kyoto-info">
           <span>Kyoto is a magical city like nowhere else.</span>
-          <span>As an imperial capital of Japan for over 1000 years (until 1868),</span>
+          <span>
+            As an imperial capital of Japan for over 1000 years (until 1868),
+          </span>
           <span>it is the heart of Japanese culture.</span>
-          <span>Kyoto is Japan in a nutshell. It's the cultural and historical heart</span>
+          <span>
+            Kyoto is Japan in a nutshell. It's the cultural and historical heart
+          </span>
           <span>of the country.</span>
         </p>
-      <div className="links">
-        <Button text="Visit the City" />
-      </div>
+        <div className="links">
+          <Button text="Visit the City" />
+          <div className="watch-video">
+            <PlayIcon />
+            <span>Watch the Video</span>
+          </div>
+        </div>
       </div>
     </>
   );
---
+++ src/icon-components/PlayIcon.js
@@ -0,0 +1,21 @@
+import "./PlayIcon.css";
+
+function PlayIcon() {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        width="50"
+        height="50"
+        viewBox="0 0 50 50"
+        style={{ fill: "#fff", opacity: "100%" }}
+      >
+        <path d="M25,2C12.318,2,2,12.317,2,25s10.318,23,23,23s23-10.317,23-23S37.682,2,25,2z M18.042,36.034l-0.083-21.996L37,24.964 L18.042,36.034z"></path>
+      </svg>
+    </>
+  );
+}
+
+export default PlayIcon;
