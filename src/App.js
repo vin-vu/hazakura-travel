c8c30718832dc065d7beec3c3433e352f011c745
vin-vu <vince.hvu@gmail.com>
2021-11-15 23:07:10 -0800

    Created Kyoto component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Homepage from './components/Homepage/Homepage';
+import Kyoto from './components/Kyoto/Kyoto';
 
 function App() {
   return (
     <>
       <Homepage />
+      <Kyoto />
     </>
   );
 }
---
+++ src/components/Kyoto/Kyoto.js
@@ -0,0 +1,11 @@
+import "./Kyoto.css";
+
+function Kyoto() {
+  return (
+    <div className="kyoto-container">
+      <p>hello</p>
+    </div>
+  )
+}
+
+export default Kyoto;
