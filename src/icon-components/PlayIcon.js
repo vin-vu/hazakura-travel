7a1aff019b1ef3e1068037af1617d663272ccb83
vin-vu <vince.hvu@gmail.com>
2021-11-15 21:41:26 -0800

    Fixed play button and link styling

--- src/icon-components/PlayIcon.js
+++ src/icon-components/PlayIcon.js
@@ -2,7 +2,7 @@ import "./PlayIcon.css";
 
 function PlayIcon() {
   return (
-    <>
+    <div className="play-icon">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         x="0px"
@@ -14,7 +14,7 @@ function PlayIcon() {
       >
         <path d="M25,2C12.318,2,2,12.317,2,25s10.318,23,23,23s23-10.317,23-23S37.682,2,25,2z M18.042,36.034l-0.083-21.996L37,24.964 L18.042,36.034z"></path>
       </svg>
-    </>
+    </div>
   );
 }
 
