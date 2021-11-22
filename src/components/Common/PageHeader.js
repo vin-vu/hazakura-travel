af0460db850c700464aff2ea0445227512fc89a8
vin-vu <vince.hvu@gmail.com>
2021-11-21 22:58:43 -0800

    Displayed paragraph and some styling for PageHeader

--- src/components/Common/PageHeader.js
+++ src/components/Common/PageHeader.js
@@ -2,9 +2,13 @@ import "./PageHeader.css";
 
 function PageHeader({ header1, title, paragraph1, paragraph2 }) {
   return (
-    <div className="page-header">
-      <span className="header-1">{header1}</span>
-      <span className="title">{title}</span>
+    <div className="page-info">
+      <div className="page-header">
+        <span className="header-1">{header1}</span>
+        <span className="title">{title}</span>
+      </div>
+      <p className="paragraph 1">{paragraph1}</p>
+      <p className="paragraph 2">{paragraph2}</p>
     </div>
   );
 }
