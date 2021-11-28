d7107b832d58206db5e42452b1f52b261f28c9db
vin-vu <vince.hvu@gmail.com>
2021-11-27 23:35:26 -0800

    Fix right section image into div

--- src/components/Common/RightSection.js
+++ src/components/Common/RightSection.js
@@ -2,9 +2,9 @@ import "./RightSection.css";
 
 function RightSection({image}) {
   return (
-    <>
+    <div className="right-section">
       <img src={image} alt="kyoto-street" />
-    </>
+    </div>
   )
 }
 
