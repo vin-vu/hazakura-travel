966e812ffdb86a7facad79a92d743b658fda2d32
vin-vu <vince.hvu@gmail.com>
2021-12-06 23:25:45 -0800

    Updated kanji colors

--- src/components/Common/RightSection.js
+++ src/components/Common/RightSection.js
@@ -4,7 +4,7 @@ import Kanji from "./Kanji.js";
 function RightSection({image}) {
   return (
     <div className="right-section">
-      <Kanji text={"京都市"} />
+      <Kanji className={"red-京都市"}text={"京都市"} />
       <img src={image} alt="kyoto-street" />
     </div>
   )
