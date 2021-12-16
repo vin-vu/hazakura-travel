a532de9398a3b8d49d10be29866caf636f622cca
vin-vu <vince.hvu@gmail.com>
2021-12-15 21:42:27 -0800

    Created text folder and file to store text

--- src/components/PageStyle2/PageStyle2.js
+++ src/components/PageStyle2/PageStyle2.js
@@ -1,15 +1,13 @@
 import PageInfo from "../Common/PageInfo";
 import './PageStyle2.css'
 
-function PageStyle2() {
-
-  // deal with only 1 paragraph bug
+function PageStyle2({header1, title}) {
 
 const paragraph1 = "There are so many things to do in Kyoto - you can visit golden temples and vermillion shrines, stroll through Zen rock gardens and swaying bamboo forests, slurp down bowls of ramen and participate in graceful tea ceremonies."
 
   return (
     <div className="page-style-2">
-      <PageInfo header1="Kyoto" title="IMMORTAL RICHES" paragraph1={paragraph1}/>
+      <PageInfo header1={header1} title={title} paragraph1={paragraph1}/>
     </div>
   )
 }
---
+++ src/text/Text.js
@@ -0,0 +1,13 @@
+export default {
+  text: [
+    {
+      header: ['GOLDEN SHADOW']
+    },
+    {
+      title: ['Kyoto']
+    }, 
+    {
+      paragraph: ['Kyoto (京都, Kyōto) served as Japan\'s capital and the emperor\'s residence from 764 to 1868. It is one of the country\'s 10 largest cities with a population of 1.5 million and a modern face.']
+    }
+  ]
+}
