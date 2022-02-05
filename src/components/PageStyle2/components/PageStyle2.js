bb5c7d41f3b03584efe9043048442245f4cbb230
vin-vu <vince.hvu@gmail.com>
2022-02-04 18:03:39 -0800

    Added kiyomizu dera card and flex wrap property

--- src/components/PageStyle2/components/PageStyle2.js
+++ src/components/PageStyle2/components/PageStyle2.js
@@ -3,7 +3,8 @@ import "../styles/PageStyle2.css";
 import Text from "../../../text/text.json";
 import Card from "./Card";
 import Kinkakuji from "../../../images/kinkakuji.jpg";
-import Fushimi from "../../../images/Fushimi Inari Shrine.jpg";
+import Fushimi from "../../../images/fushimi-inari-shrine.jpg";
+import Kiyomizu_dera_gate from "../../../images/kiyomizu-dera-gate.jpg";
 
 function PageStyle2() {
   return (
@@ -22,6 +23,7 @@ function PageStyle2() {
       <div className="cards-container">
         <Card img={Kinkakuji} alt="Kinkakuji" title="Kinkakuji" info="A Zen Temple in Northern Tokyo whose top 2 floors are completely covered in golden leaf." />
         <Card img={Fushimi} alt="Fushimi" title="Fushimi Inari Shrine" info="The most important of several thousands of shrines dedicated to Inari, the Shinto god of rice." />
+        <Card img={Kiyomizu_dera_gate} alt="Kiyomizu_dera_gate" title="Kiyomizu-dera" info="One of the most celebrated temples in Japan. Was added to the list of UNESECO world heritage sies." />
       </div>
     </div>
   );
